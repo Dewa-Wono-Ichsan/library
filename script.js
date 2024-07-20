5ab30a68552d7e4732667e1edac3d8db9356e591
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-07-21 05:35:00 +0700

    Add query selector to title book input and event listener to add book button
    Make user input on title book then click add button to receive value. Value of input element with id title-book will be used in constructor object book.
    
    From constructor object function, add book will push the object to array of variable myLibrary when user click the button add book.
    
    Variable myLibrary will receive variable object that contain key of title with value that entered by user in input element with id title-book.

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const myLibrary = []
 
+const selectorInput = {
+
+    'title book': document.querySelector('input#title-book'),
+    'submit book': document.querySelector('button[type=submit]'),
+
+}
+
 function Book(title, author, pages, read) {
+
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
+    
 }
+
+function AddBookToLibrary() {
+
+    myLibrary.push(
+        new Book(selectorInput['title book'].value)
+    )
+    event.preventDefault()
+
+}
+
+selectorInput['submit book'].addEventListener('click', AddBookToLibrary)
