65002a8fde44cf0d0b6235e81115e79734f977e1
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-07-23 07:48:32 +0700

    Add div with class container-book and display information book in user window browser
    Make div with class container-information-book to contain input value for user to be displayed in their window. Nest container-information-book in container-book. Every add book button is clicked, data from object book myLibrary displayed in container-information-book.
    
    Make document object model for title, author, pages, reading progress, language, genre in div element with class container-information-book.
    
    Make div with unique class for every input information to set style design and layout document.

--- script.js
+++ script.js
@@ -10,11 +10,20 @@ function Book(title, author, pages, read, language, genre) {
     this.genre = genre
 }
 
-Book.prototype.sayHello = {
-    say: function() {alert('hello world!')},
-}
-
 function addBookToLibrary() {
+
+    const containerBook = document.querySelector('div.container-book')
+    const containerInformationBook = document.createElement('div')
+    containerInformationBook.classList.toggle('container-information-book')
+    containerBook.appendChild(containerInformationBook)
+
+    function displayBook(structureDoc, classDoc, contentDoc, container) {
+
+        const create = document.createElement(`${structureDoc}`)
+        create.classList.toggle(`${classDoc}`)
+        create.textContent = myLibrary[myLibrary.length-1][`${contentDoc}`]
+        container.appendChild(create)
+    }
 
     const selectorInput = {
     
@@ -27,7 +36,9 @@ function addBookToLibrary() {
     }
     
     myLibrary.push(
+
         new Book(
+
             selectorInput['title book'].value,
             selectorInput['author book'].value,
             selectorInput['pages book'].value,
@@ -37,6 +48,13 @@ function addBookToLibrary() {
         )
     )
 
+    displayBook('div', 'title-book', 'title', containerInformationBook)
+    displayBook('div', 'author-book', 'author', containerInformationBook)
+    displayBook('div', 'pages-book', 'pages', containerInformationBook)
+    displayBook('div', 'read-progress-book', 'read', containerInformationBook)
+    displayBook('div', 'language-book', 'language', containerInformationBook)
+    displayBook('div', 'genre-book', 'genre', containerInformationBook)
+
     selectorInput['title book'].value = ''
     selectorInput['author book'].value = ''
     selectorInput['pages book'].value = ''
@@ -44,7 +62,6 @@ function addBookToLibrary() {
     selectorInput['genre book'].value = ''
 
     event.preventDefault()
-
 }
 
 document.querySelector('button[type=submit]').addEventListener('click', addBookToLibrary)
